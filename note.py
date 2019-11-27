16e337575e72be23ad390895bbc22918f2756f34
Tyler Eikenberg <tylereikenberg8195@gmail.com>
2019-11-27 13:22:17 -0500

    created greeting intro

--- note.py
+++ note.py
@@ -1,8 +1,10 @@
 from peewee import *
 from datetime import date
+import pyfiglet
 db = PostgresqlDatabase('note', user='postgres', password='',
                         host='localhost', port=5432)
 db.connect() 
+# pipenv run python note.py 
 
 class BaseModel(Model):
     class Meta:
@@ -13,10 +15,20 @@ class BaseModel(Model):
 # model for notes
 
 class Person(BaseModel):
-    name = CharField()
+    full_name = CharField()
 
 class Note(BaseModel):
     date_created = DateField()
-    note-content = TextField()
+    note_content = TextField()
 
 db.create_tables([Person, Note])
+
+# prompt user for full name, create Person from name
+
+def app_intro():
+    title = pyfiglet.figlet_format("PyThoughts")
+    subtitle = "--A Python note-taking application--\n-\n-"
+    print(title)
+    print(subtitle)
+app_intro()
+# def get_user()
